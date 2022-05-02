b6114160046992af4e9e61ba249b94d86d028cf7
Rowings <zakariaazarkan@gmail.com>
2022-05-02 14:49:25 +0200

    ex 2, heure actuelle

---
+++ exo 2/script.js
@@ -0,0 +1,26 @@
+// Avant d'afficher le temps passé sur le site je me suis entrainé en affichant l'heure afin de cerner le principe 
+
+const horloge = function() {
+    
+    let date = new Date();
+
+    let heure = date.getHours();
+    let minutes = date.getMinutes();
+    let secondes = date.getSeconds();
+
+
+
+     if (secondes < 10) {
+     secondes = "0" + secondes;
+     }
+     if (minutes < 10) {
+     minutes = "0" + minutes;
+     }
+
+    let affichage = heure + ":" + minutes + ":" + secondes;
+    document.body
+    document.querySelector("#horloge").innerText = affichage;
+
+}
+
+window.setInterval(horloge, 1000);
